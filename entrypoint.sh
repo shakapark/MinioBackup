fa54dbe2db435cbbbc3ddcc7b682eeefa53fb6bb
Shakapark <shakapark1994@gmail.com>
2018-03-01 14:19:21 +0100

    Remove '_' in date

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ function backupMinioToMinio() {
   mc config host add minioBackup http://${MINIO_BACKUP} ${MINIO_BACKUP_ACCESS_KEY} ${MINIO_BACKUP_SECRET_KEY}
   echo "Adding host minioBackup : ${MINIO_BACKUP}"
   
-  date=$(date +"%d_%m_%Y")
+  date=$(date +"%d%m%Y")
   
   mc mb minioBackup/Backup${date}
   echo "Create new bucket on ${MINIO_BACKUP}: Backup${date}"
