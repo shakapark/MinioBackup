799ee5aa659d07d4f7a45b5c868c162acb577d6a
Shakapark <shakapark1994@gmail.com>
2018-03-01 08:18:39 +0100

    Add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+function backupMinioToMinio() {
+
+  mc config host add minio http://${MINIO_ENDPOINT} ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
+  echo "Adding host minio : ${MINIO_ENDPOINT}"
+  
+  mc config host add minioBackup http://${MINIO_BACKUP} ${MINIO_BACKUP_ACCESS_KEY} ${MINIO_BACKUP_SECRET_KEY}
+  echo "Adding host minioBackup : ${MINIO_BACKUP}"
+  
+  date=$(date +"%d_%m_%Y")
+  
+  pc mb minioBackup/Backup${date}
+  echo "Create new bucket on ${MINIO_BACKUP}: Backup${date}"
+  
+  mc cp -r minio minioBackup/Backup${date}
+  echo "Copy ${MINIO_ENDPOINT} to ${MINIO_BACKUP}/Backup${date}"
+
+}
+
+backupMinioToMinio
