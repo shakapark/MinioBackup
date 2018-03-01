7adba10a502fc22cc8b7c12981d6d9000b9cbdff
Shakapark <shakapark1994@gmail.com>
2018-03-01 17:36:20 +0100

    Little test

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,9 @@ function backupMinioToMinio() {
   BUCKETS=$(mc --json ls minio | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1)
   echo ${BUCKETS}| while read BUCKET
   do
-    mc cp -r minio/${BUCKET} minioBackup/backup${date}
-    echo "Copy ${MINIO_ENDPOINT}/${BUCKET} to ${MINIO_BACKUP}/backup${date}"
+    echo "bucket: ${BUCKET}\n"
+    #mc cp -r minio/${BUCKET} minioBackup/backup${date}
+    #echo "Copy ${MINIO_ENDPOINT}/${BUCKET} to ${MINIO_BACKUP}/backup${date}"
   done
   
   exit 0
