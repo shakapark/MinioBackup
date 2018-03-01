c8032c96b907330046f087c018b1b2c2376e7e3a
Shakapark <shakapark1994@gmail.com>
2018-03-01 14:25:25 +0100

    Change 'B' to 'b' in bucket name and add '-' in date

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ function backupMinioToMinio() {
   mc config host add minioBackup http://${MINIO_BACKUP} ${MINIO_BACKUP_ACCESS_KEY} ${MINIO_BACKUP_SECRET_KEY}
   echo "Adding host minioBackup : ${MINIO_BACKUP}"
   
-  date=$(date +"%d%m%Y")
+  date=$(date +"%d-%m-%Y")
   
-  mc mb minioBackup/Backup${date}
+  mc mb minioBackup/backup${date}
   echo "Create new bucket on ${MINIO_BACKUP}: Backup${date}"
   
   mc cp -r minio minioBackup/Backup${date}
