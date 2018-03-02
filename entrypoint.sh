404108512cc3186006280af95592222fc2d4dbd0
Shakapark <shakapark1994@gmail.com>
2018-03-02 10:16:10 +0100

    Remove Old Bucket Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,17 @@ set -e
 
 function backupMinioToMinio() {
 
-  mc config host add minio http://${MINIO_ENDPOINT} ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
-  mc config host add minioBackup http://${MINIO_BACKUP} ${MINIO_BACKUP_ACCESS_KEY} ${MINIO_BACKUP_SECRET_KEY}
-  
-  date=$(date +"%d-%m-%Y")
-  mc mb minioBackup/backup${date}
-  
-  BUCKETS=$(mc --json ls minio | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " " "\n")
-  for BUCKET in ${BUCKETS}
+  DATE=$(date +"%d-%m-%Y")
+  mc mb $DST/backup$DATE
+
+  BUCKETS=$(mc --json ls $SRC | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " " "\n")
+  for BUCKET in $BUCKETS
   do
-    mc cp -r minio/${BUCKET} minioBackup/backup${date}
+    mc cp -r $SRC/$BUCKET $DST/$DATE
   done
+
+  DATE=$(date -d $DATE - $RETENTION +"%d-%m-%Y")
+  echo $DATE
 }
 
 echo "Start Backup"
