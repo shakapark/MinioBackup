231bc856cc6d427c1a5daf916f37d7c34beae127
Shakapark <shakapark1994@gmail.com>
2018-03-08 12:06:49 +0100

    Add more info

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,24 @@
 function backupMinioToMinio() {
   echo "Starting Backup"
 
+  DATE=$(date -d "$RETENTION days ago" +"%d-%m-%Y")
+  mc rm --recursive --force $DST/$DATE
+
   DATE=$(date +"%d-%m-%Y")
   mc mb $DST/$DATE
+  echo $?
+
 
   BUCKETS=$(mc --json ls $SRC | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " " "\n")
   for BUCKET in $BUCKETS
   do
     mc cp -r $SRC/$BUCKET $DST/$DATE
+    echo $?
+    if [ $? != 0 ]
+    then
+      exit 1
+    fi
   done
-
-  DATE=$(date -d "$RETENTION days ago" +"%d-%m-%Y")
-  mc rm --recursive --force $DST/$DATE
 
   echo "Backup Done"
 }
